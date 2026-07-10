809d776e43d8e59bec4c75a1386eecd0c2af3fd4
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2026-07-10 14:51:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { HomePage } from "./pages/HomePage";
@@ -12,24 +12,50 @@ import { PrivacyPage } from "./pages/PrivacyPage";
 import { TermsPage } from "./pages/TermsPage";
 import ScrollToTop from "./components/ScrollToTop";
 
+// LMS Pages
+import { LmsLandingPage } from "./pages/LmsLandingPage";
+import { LmsCoursesPage } from "./pages/LmsCoursesPage";
+import { LmsCourseDetailsPage } from "./pages/LmsCourseDetailsPage";
+import { LmsStudentDashboard } from "./pages/LmsStudentDashboard";
+import { LmsCoursePlayer } from "./pages/LmsCoursePlayer";
+import { LmsAdminDashboard } from "./pages/LmsAdminDashboard";
+
+function MainAppLayout() {
+  const location = useLocation();
+  const isPlayer = location.pathname.startsWith("/lms/player");
+  const isAdmin = location.pathname.startsWith("/lms/admin");
+
+  return (
+    <div className="min-h-screen bg-white selection:bg-indigo-100 flex flex-col justify-between">
+      <ScrollToTop />
+      {!isPlayer && !isAdmin && <Navbar />}
+      
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/internship" element={<InternshipPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          
+          {/* LMS Routes */}
+          <Route path="/lms" element={<LmsLandingPage />} />
+          <Route path="/lms/courses" element={<LmsCoursesPage />} />
+          <Route path="/lms/course/:id" element={<LmsCourseDetailsPage />} />
+          <Route path="/lms/dashboard" element={<LmsStudentDashboard />} />
+          <Route path="/lms/player/:courseId" element={<LmsCoursePlayer />} />
+          <Route path="/lms/admin" element={<LmsAdminDashboard />} />
+        </Routes>
+      </main>
+
+      {!isPlayer && !isAdmin && <Footer />}
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white selection:bg-indigo-100">
-        <ScrollToTop />
-        <Navbar />
-        
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/internship" element={<InternshipPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-          </Routes>
-        </main>
-
-        <Footer />
-      </div>
+      <MainAppLayout />
     </Router>
   );
 }
