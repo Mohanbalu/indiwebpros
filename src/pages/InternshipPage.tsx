891e0a1e4184b9da9102ce0952238e5b104a5e5a
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2026-09-02 18:53:38 +0530

    Update InternshipPage.tsx

--- src/pages/InternshipPage.tsx
+++ src/pages/InternshipPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { InternshipSection } from "../components/InternshipSection";
+import { CertificateVerificationBanner } from "../components/certificate/CertificateVerificationBanner";
 
 export function InternshipPage() {
   return (
     <div className="w-full min-h-screen bg-slate-950">
       <InternshipSection />
+      <CertificateVerificationBanner />
     </div>
   );
 }
